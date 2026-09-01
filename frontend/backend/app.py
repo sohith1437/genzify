362d88e1d2263f854a076a108b3a5ead547c142a
sohith1437 <sohithnagendra80@gmail.com>
2026-09-01 12:43:27 +0530

    Prepare backend for deployment

--- frontend/backend/app.py
+++ frontend/backend/app.py
@@ -77,4 +77,11 @@ Original text:
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+
+    port = int(os.environ.get("PORT", 5000))
+
+    app.run(
+        host="0.0.0.0",
+        port=port
+    )
